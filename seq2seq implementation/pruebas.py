dde0fb0e69f1ba1e1cce110f5c69cc0f445bc6d3
Fiquinho <ficobogado@gmail.com>
2017-06-11 20:51:37 -0300

    finished seq2seq implementation

---
+++ seq2seq implementation/pruebas.py
@@ -0,0 +1,5 @@
+data = 'abqqs\nnpy\nbjluw\nbqv\naikl\naddmt\ndegjppx\nnpsv\nhlouz\ncklmq\neir\nv\nlp\naben\nackl\nahht\nf\nksstzz\nlqv\neknqtz\n'
+
+set_words = set([character for line in data.split('\n') for character in line])
+
+print(set_words)
